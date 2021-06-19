73efeace89503932bdad348ce94c2e7b5d638676
kokolight <tsegal97@gmail.com>
2021-06-19 23:43:07 +0300

    updated code

--- main.py
+++ main.py
@@ -7,98 +7,180 @@ import re
 import os.path
 from os import path
 
-file_name = "video_games_ratings.csv"
-base_url = "https://www.metacritic.com"
-browse_url = base_url + '/browse/games/score/metascore/all/all/filtered?'
-columns = ['name', 'metascore', 'userscore', 'platform', 'platforms_count', 'awards_count', 'publisher', 'developer', 'release_date',
-           'mature_rating', 'positive_critic_reviews_count', 'mixed_critic_reviews_count', 'negative_critic_reviews_count',
-           'positive_user_reviews_count', 'mixed_user_reviews_count', 'negative_user_reviews_count']
+# Retrieving Data from metacritic "https://metacritic.com" (Main) some other details from API "https://rawg.io"
 
-month_dict = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7',
-              'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
+# Data we scrape
+"""
+- Name
+- Release Date
+- Metascore
+- Userscore
+"""
+# Data we get from API
+"""
+- Singleplayer - True/False
+- Multiplayer - True/False
+- Achievements count
+- Youtube related videos count
+- Average playtime
+- Is part of a series
+- Publisher
+- Developer
+- Number of consoles
+- Maturaty Rating
+"""
 
-soup = BeautifulSoup('html.parser')
+
+def get_game_rawg_details(game_name):
+    RAWG_URL = f"https://api.rawg.io/api/games/{game_name}?key={RAWG_API_KEY}"
+    data = requests.get(RAWG_URL, headers=HEADERS).json()
+    if "redirect" in data:
+        game_name = data["slug"]
+        RAWG_URL = f"https://api.rawg.io/api/games/{game_name}?key={RAWG_API_KEY}"
+        data = requests.get(RAWG_URL, headers=HEADERS).json()
+    elif "detail" in data:
+        return []
+
+    game_details = {
+        "achievements_count": data["parent_achievements_count"],
+        "avg_playtime": data["playtime"],
+        "youtube_videos_count": data["youtube_count"],
+        "singleplayer": False,
+        "multiplayer": False,
+        "game_series_count": data["game_series_count"],
+        "maturaty_rating": data["esrb_rating"]["name"][0] if data["esrb_rating"] else None,
+        "platforms_count": len(data["platforms"]),
+        "publisher": data["publishers"][0]["name"] if len(data["publishers"]) else None,
+        "developer": data["developers"][0]["name"] if len(data["developers"]) else None,
+        "ratings_count": data["ratings_count"],
+    }
+    for tag in data["tags"]:
+        if tag["slug"] == "singleplayer":
+            game_details["singleplayer"] = True
+        if tag["slug"] == "multiplayer":
+            game_details["multiplayer"] = False
+    return game_details
+
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+}
+RAWG_API_KEY = "1a30728a7348454fa81a4a98e057442d"
+FILE_NAME = "video_games_ratings.csv"
+BASE_URL = "https://www.metacritic.com"
+BROWSE_URL = BASE_URL + "/browse/games/score/metascore/all/all/filtered?"
+COLUMNS = [
+    "game_name",
+    "metascore",
+    "userscore",
+    "platforms_count",
+    "awards_count",
+    "publisher",
+    "developer",
+    "release_date",
+    "maturaty_rating",
+    "singleplayer",
+    "multiplayer",
+    "youtube_videos_count",
+    "achievements_count",
+    "ratings_count",
+    "average_playtime",
+    "game_series_count",
+]
+
+month_dict = {
+    "January": "1",
+    "February": "2",
+    "March": "3",
+    "April": "4",
+    "May": "5",
+    "June": "6",
+    "July": "7",
+    "August": "8",
+    "September": "9",
+    "October": "10",
+    "November": "11",
+    "December": "12",
+}
+
+
+def get_game_data(game_slug, game_name, metascore, userscore, release_date):
+    rawg_details = get_game_rawg_details(game_slug)
+    if len(rawg_details) == 0:
+        return []
+
+    # meta_score = soup.find('div', class_="metascore_w").text
+    return {
+        "game_name": game_name,
+        "release_date": release_date,
+        "metascore": metascore,
+        "userscore": userscore,
+        "publisher": rawg_details["publisher"],
+        "developer": rawg_details["developer"],
+        "maturaty_rating": rawg_details["maturaty_rating"],
+        "platforms_count": rawg_details["platforms_count"],
+        "average_playtime": rawg_details["avg_playtime"],
+        "achievements_count": rawg_details["achievements_count"],
+        "youtube_videos_count": rawg_details["youtube_videos_count"],
+        "singleplayer": rawg_details["singleplayer"],
+        "multiplayer": rawg_details["multiplayer"],
+        "ratings_count": rawg_details["ratings_count"],
+        "game_series_count": rawg_details["game_series_count"],
+    }
 
 
 def metacritic_games_data(start_page, end_page):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
     index = 1
     game_records = []
 
-    current_data = pd.read_csv(file_name, index_col=0) if path.exists(
-        file_name) else pd.DataFrame(columns=columns)
-
+    current_data = pd.read_csv(FILE_NAME, index_col=0) if path.exists(FILE_NAME) else pd.DataFrame(columns=COLUMNS)
+    soup = BeautifulSoup(features="lxml")
     # Get all games in all pages
     for i in range(start_page, end_page):
-        page_url = browse_url+'page={0}'.format(i)
-        req = requests.get(page_url, headers=headers)
-        soup.append(bs4.BeautifulSoup(req.text))
+        page_url = BROWSE_URL + "page={0}".format(i)
+        req = requests.get(page_url, headers=HEADERS)
+        soup.append(bs4.BeautifulSoup(req.text, "lxml"))
 
-    games_objects = soup.find_all("td", class_="clamp-summary-wrap")
+    trs = set(soup.select("tr"))
+    spacers = set(soup.select("tr.spacer"))
+    games = trs - spacers
 
     # For each game
-    for game in games_objects:
-        record = {}
-        title_obj = game.find('a', class_="title")
-        title = title_obj.text
-
+    for game in games:
+        game_name = game.select("h3")[0].text
         # Already exists
-        if (not current_data.empty) and (title in current_data['name'].values):
-            print(f"Already in dataframe - {title}")
+        if (not current_data.empty) and (game_name in current_data["game_name"].values):
+            print(f"Already in dataframe - {game_name}")
+            index = index + 1
             continue
-
-        href = title_obj.get("href")
-        meta_score = game.find('div', class_="metascore_w").text
-        platform = game.find("span", class_="data").text.strip()
-        date = game.find(
-            "div", class_="clamp-details").find_all("span")[2].text.replace(",", "")
+        metascore = game.select("div.metascore_w.game")[0].text
+        userscore = game.select("div.metascore_w.user.game")[0].text
+        date = game.select("div.clamp-details")[0].find_all("span")[2].text.replace(",", "")
         date_splitted = date.split(" ")
         release_date = "{day}-{month}-{year}".format(
-            day=date_splitted[1], month=month_dict[date_splitted[0]], year=date_splitted[2])
+            day=date_splitted[1], month=month_dict[date_splitted[0]], year=date_splitted[2]
+        )
+        game_href = game.find("a", class_="title", href=True)["href"]
+        game_slug = re.findall(".*\/(.*)$", game_href)[0]
 
-        # add the values
-        record.update({'name': title, 'metascore': meta_score,
-                       'platform': platform, 'release_date': release_date})
+        print(f"Game: {game_name}")
 
-        # in game section
-        game_url = base_url+''+href
-        game_req = requests.get(game_url, headers=headers)
-        print("Progress: {:.2f}%".format(
-            (index/100*(end_page-start_page))*100))
-        game_soup = BeautifulSoup(game_req.text, 'html.parser')
-        print(game_url)
-        user_score = game_soup.select('div.feature_userscore')[0].select(
-            '.metascore_w.user.large.game')[0].text
-        publisher = game_soup.select('li.publisher')[0].find("a").text.strip()
-        platforms_count = len(game_soup.select("li.product_platforms")[0].find_all(
-            "a"))+1 if game_soup.select("li.product_platforms") else 1
-        developer = game_soup.select('li.developer')[0].find("a").text
-        awards_count = len(game_soup.find_all("div", class_="ranking_title"))
-        mature_rating = game_soup.select("li.product_rating")[0].select(
-            "span.data")[0].text if game_soup.select("li.product_rating") else ''
+        game_object = get_game_data(game_slug, game_name, metascore, userscore, release_date)
+        if len(game_object):
+            game_records.append(game_object)
+        else:
+            print(f"Couldn't get game details: {game_name}")
 
-        # Reviews
-        # game_url = base_url+''+href+'/user-reviews'
-        # game_req = requests.get(game_url, headers=headers)
-        # game_soup = BeautifulSoup(game_req.text, 'html.parser')
-        critic_reviews_count = game_soup.select("div.metascore_summary")[0].find(
-            "span", class_="count").find("a").find("span").text.strip()
-        # user_reviews_count = re.search(r"\d+", game_soup.select("div.side_details")[0].find("span", class_="count").find("a").text).group(0)
-
-        # add the values per game
-        record.update({'userscore': user_score, 'awards_count': awards_count, 'publisher': publisher, 'developer': developer,
-                       'critic_reviews_count': critic_reviews_count, 'platforms_count': platforms_count, 'mature_rating': mature_rating})
-        game_records.append(record)
+        print("Progress: {:.2f}%".format((index / len(games)) * 100))
         index = index + 1
 
     new_data = pd.DataFrame(game_records)
 
     if not new_data.empty:
         current_data = current_data.append(new_data, ignore_index=True)
-        current_data.to_csv("./video_games_ratings.csv",  mode='w')
+        current_data.to_csv("./video_games_ratings.csv", mode="w")
     return current_data
 
 
-df = metacritic_games_data(2, 3)
-df
+df = metacritic_games_data(1, 30)
+print(df)
